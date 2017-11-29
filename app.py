a39ec8c2d2798a43c2ed53ac70247c08b7dc94ff
KSeeker5 <alexknipper@comcast.net>
2017-11-29 16:06:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def webhook():
   log('Recieved {}'.format(data))
 
   # We don't want to reply to ourselves!
-  if data['name'] != 'apnorton-test-bot':
+  if data['name'] != 'Infinite':
     msg = '{}, you sent "{}".'.format(data['name'], data['text'])
     send_message(msg)
 
